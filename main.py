1ab3f01d463d2ddc9c3b5900fbef42398cff0c19
Alex Pereira <alex.pereira.6464@gmail.com>
2023-01-03 17:51:13 -0500

    Updated the tag detection Updated the tag detection methods and the way the results are handled
    Signed-off-by: Alex Pereira <alex.pereira.6464@gmail.com>

--- main.py
+++ main.py
@@ -6,50 +6,31 @@ import cv2 as cv
 # Import Classes
 from camera import USBCamera
 from apriltags import Detector
+from communications import NetworkCommunications
 
 # Instance creation
-detector = Detector()
+nComms   = NetworkCommunications()
+detector = Detector(nComms)
 
-# Create VideoCapture 0 and get its properties
-camera0        = USBCamera(0)
-cap0           = camera0.autoResize()
-cam0resolution = camera0.getResolution()
-ret, cam0Matrix, cam0distortion, rvecs, tvecs = camera0.calibrateCamera(cap0)
-
-def undistort(stream, cameraMatrix, distortion, resolution: tuple):
-    """
-    Undistorts an image using cv.undistort()
-    @param stream
-    @param cameraMatrix
-    @param cameraDistortion
-    @param cameraResolution (width, height)
-    @return undistortedStream
-    """
-    # Creates a cameraMatrix
-    newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, distortion, resolution, 1, resolution)
-
-    # Undistorts the image
-    undistortedStream = cv.undistort(stream, cameraMatrix, distortion, None, newCameraMatrix)
-
-    # Crops the image
-    x, y, w, h = roi
-    undistortedStream = undistortedStream[y:y+h, x:x+w]
-
-    return undistortedStream
+# Creates a VideoCapture and gets its properties
+camera        = USBCamera(0)
+cap           = camera.autoResize()
+camresolution = camera.getResolution()
+ret, camMatrix, camdistortion, rvecs, tvecs = camera.calibrateCamera(cap)
 
 # Main loop
-while (cap0.isOpened() == True):
+while (cap.isOpened() == True):
     # Read the capture
-    sucess, stream = cap0.read()
+    sucess, stream = cap.read()
  
     # Flips the image
     cv.flip(stream, 1)
 
     # Undistorts the image
-    stream = undistort(cam0Matrix, cam0distortion, cam0resolution)
+    stream = detector.undistort(camMatrix, camdistortion, camresolution)
 
-    # Runs apriltag detection on the image
-    results, stream = detector.dtDetectTags(stream, cam0Matrix, 3, 1, False)
+    # Runs apriltag detection on the undistorted image
+    results, stream = detector.detectTags(stream, camMatrix, 2, 1, False)
 
     # Display the capture
     cv.imshow("Stream", stream)
@@ -58,5 +39,5 @@ while (cap0.isOpened() == True):
     if ( cv.waitKey(1) == ord("q") ):
         print("Process Ended by User")
         cv.destroyAllWindows()
-        cap0.release()
+        cap.release()
         break
