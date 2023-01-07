8a56f9a444bb17b42aaf8c3ad2722177895311f9
Alex Pereira <alex.pereira.6464@gmail.com>
2023-01-06 22:29:48 -0500

    Update to Install.sh Updated Install.sh to reflect changes to the installation procedure.
    Signed-off-by: Alex Pereira <alex.pereira.6464@gmail.com>

--- Install.sh
+++ Install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Back out to the home directory
+cd
+
 # Get system updates
 sudo apt-get update
 
@@ -9,7 +12,7 @@ sudo apt-get -y upgrade
 # Install python 3.10 if it is not already install
 sudo apt-get install -y python3.10
 
-# Install python3-pip, cmake, ninja, and opencv (c++)
+# Install git, python3-pip, cmake, ninja, and opencv (c++)
 sudo apt-get install -y git python3-pip cmake ninja-build libopencv-dev
 
 # Install all needed pip libraries
