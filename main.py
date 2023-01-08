26b68b3418eaa1ff2960df28e22ec2e76c6e15c1
Alex Pereira <alex.pereira.6464@gmail.com>
2023-01-08 12:36:41 -0500

    Forgot to edit main.py
    Signed-off-by: Alex Pereira <alex.pereira.6464@gmail.com>

--- main.py
+++ main.py
@@ -20,24 +20,24 @@ ret, camMatrix, camdistortion, rvecs, tvecs = camera.calibrateCamera(cap)
 
 # Main loop
 while (cap.isOpened() == True):
-    # Read the capture
-    sucess, stream = cap.read()
+	# Read the capture
+	sucess, stream = cap.read()
 
-    # Undistorts the image
-    stream = detector.undistort(stream, camMatrix, camdistortion, camresolution)
+	# Undistorts the image
+	stream = detector.undistort(stream, camMatrix, camdistortion, camresolution)
 
-    # Runs apriltag detection on the undistorted image
-    results, stream = detector.detectTags(stream, camMatrix, 3, 0, False)
+	# Runs apriltag detection on the undistorted image
+	results, stream = detector.detectTags(stream, camMatrix, 3, 0, False)
 
-    # Flips the stream for better viewing
-    stream = cv.flip(stream, 1)
+	# Flips the stream for better viewing
+	stream = cv.flip(stream, 1)
 
-    # Display the capture
-    cv.imshow("Stream", stream)
- 
-    # Press q to end the program
-    if ( cv.waitKey(1) == ord("q") ):
-        print("Process Ended by User")
-        cv.destroyAllWindows()
-        cap.release()
-        break
+	# Display the capture
+	cv.imshow("Stream", stream)
+
+	# Press q to end the program
+	if ( cv.waitKey(1) == ord("q") ):
+		print("Process Ended by User")
+		cv.destroyAllWindows()
+		cap.release()
+		break
