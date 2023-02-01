155e041e2d4240f46d9df925f3510067ae3e9b14
Alex Pereira <alex.pereira.6464@gmail.com>
2023-02-01 11:09:44 -0500

    Removed pose_estimation.py Tranfered the funcationaly of pose_estimation.py to the April-Tags branch of 2023Code.
    Signed-off-by: Alex Pereira <alex.pereira.6464@gmail.com>

--- communications.py
+++ communications.py
@@ -30,31 +30,12 @@ class NetworkCommunications:
         FMSInfo = ntinst.getTable("FMSInfo")
         self.isRedAlliance = FMSInfo.getEntry("IsRedAlliance")  # Boolean
 
-        # Targetting table
-        Targetting = ntinst.getTable("Targetting")
-        self.targets = Targetting.getEntry("Targets")  # Double[]
-        self.offsets = Targetting.getEntry("Offsets")  # Double[]
-        self.drivePowers = Targetting.getEntry("DrivePowers")  # Double[]
-
-        # Create a RobotData table and its entries
-        RobotData = ntinst.getTable("RobotData")
-        self.gyroYaw     = RobotData.getEntry("GyroYaw")     # Double
-        self.detectTime  = RobotData.getEntry("DetectTime")  # Double
-        self.FLRot       = RobotData.getEntry("FLRotation")  # Double
-        self.RLRot       = RobotData.getEntry("RLRotation")  # Double
-        self.FRRot       = RobotData.getEntry("FRRotation")  # Double
-        self.RRRot       = RobotData.getEntry("RRRotation")  # Double
-        self.FLVel       = RobotData.getEntry("FLVelocity")  # Double
-        self.RLVel       = RobotData.getEntry("RLVelocity")  # Double
-        self.FRVel       = RobotData.getEntry("FRVelocity")  # Double
-        self.RRVel       = RobotData.getEntry("RRVelocity")  # Double
-        self.camOffset   = RobotData.getEntry("CameraOffset") # Double[]
-        self.targetValid = RobotData.getEntry("tv")  # Boolean
-
         # Create a TagInfo Table and its Entries
         TagInfo = ntinst.getTable("TagInfo")
-        self.bestResult   = TagInfo.getEntry("BestResult")  # Double[]
-        self.bestResultId = TagInfo.getEntry("BestResultId")  # Double
+        self.targetValid   = TagInfo.getEntry("tv")            # Boolean
+        self.bestResult    = TagInfo.getEntry("BestResult")    # Double[]
+        self.bestResultId  = TagInfo.getEntry("BestResultId")  # Double
+        self.detectionTime = TagInfo.getEntry("DetectionTime") # Double
 
         # Updates log
         Logger.logInfo("NetworkCommunications initialized")
@@ -100,15 +81,6 @@ class NetworkCommunications:
         # Sends the data
         self.bestResult.setDoubleArray(data)
 
-    def setDrivePowers(self, calcXPower: float, calcYPower: float, calcRotatePower: float):
-        """
-        Sets the drive powers.
-        @param calcXPower
-        @param calcYPower
-        @param calcRotatePower
-        """
-        self.drivePowers.setDoubleArray([calcXPower, calcYPower, calcRotatePower])
-
     def setTargetValid(self, tv: bool):
         """
         Sets if a valid target was detected.
@@ -116,156 +88,9 @@ class NetworkCommunications:
         """
         self.targetValid.setBoolean(tv)
 
-    def setDetectTimeSec(self, timeSec: float):
+    def setDetectionTimeSec(self, timeSec: float):
         """
         Sets the time when a detection was made.
         @param timeSec
         """
-        self.detectTime.setDouble(timeSec)
-    
-    def setCamOffset(self, camOffset: Transform3d):
-        """
-        Sets the camera transformation.
-        @param cameraTransform
-        """
-        # Extract distances from the Transform3d
-        x, y, z = camOffset.X(), camOffset.Y(), camOffset.Z()
-
-        # Extract rotations from the Transform3d
-        roll, pitch, yaw = camOffset.rotation().X(), camOffset.rotation().Y(), camOffset.rotation().Z()
-
-        # Set the double array
-        self.camOffset.setDoubleArray([x, y, z, roll, pitch, yaw])
-
-    def getBestResultId(self) -> int:
-        """
-        Gets the tag id of the best result.
-        @return tagId
-        """
-        return int(self.bestResultId.getDouble(-1))
-
-    def getBestResult(self) -> Pose3d:
-        """
-        Gets the best result stored in NetworkTables. 
-        @return Pose3d
-        """
-        # Gets the stored info
-        info = self.bestResult.getDoubleArray([-1, 0, 0, 0, 0, 0, 0])
-
-        # Returns a Pose3d object
-        return Pose3d(
-            Translation3d(info[1], info[2], info[3]),
-            Rotation3d(info[4], info[5], info[6])
-        )
-
-    def getTargetIds(self) -> Sequence[float]:
-        """
-        Gets the ids of the target tags.
-
-        Should correspond with the distances provided by the Offsets entry.
-        @return arrayOfTargetIds
-        """
-        return self.targets.getDoubleArray([-1])
-
-    def getTargetOffsets(self) -> Sequence[float]:
-        """
-        Gets the offsets from the tags registered as targets.
-        
-        Should correspond with the tag numbers provided by the Targets entry.
-        The data is in the format is x, y, z, roll, pitch, yaw but extends infinitely
-        @return arrayOfTransforms
-        """
-        # Gets the offsets
-        offsets = self.offsets.getDoubleArray([0, 0, 0, 0, 0, 0])
-
-        # Variables
-        transforms = []
-        length = len(offsets)
-        modLength = length % 6
-        itterate = int((length - modLength) / 6)
-
-        # Parses the results into a series of Transform3ds
-        for i in range(0, itterate):
-            print(i)
-            lowerBound = i * 6
-            upperBound = (i + 1) * 6
-            tag = offsets[lowerBound : upperBound]
-            temp = Transform3d(
-                Translation3d(tag[0], tag[1], tag[2]),
-                Rotation3d(tag[3], tag[4], tag[5])
-            )
-
-            # Adds the temp variable to the returned array
-            transforms.append(temp)
- 
-        return transforms
-
-    def getFLPosition(self) -> SwerveModulePosition:
-        """
-        Gets the SwerveModulePosition of the Front Left module.
-        @return SwerveModulePosition
-        """
-        return SwerveModulePosition(self.FLVel.getDouble(0), self.degToRotation2d(self.FLRot.getDouble(0)))
-
-    def getRLPosition(self) -> SwerveModulePosition:
-        """
-        Gets the SwerveModulePosition of the Rear Left module.
-        @return SwerveModulePosition
-        """
-        return SwerveModulePosition(self.RLVel.getDouble(0), self.degToRotation2d(self.RLRot.getDouble(0)))
-
-    def getFRPosition(self) -> SwerveModulePosition:
-        """
-        Gets the SwerveModulePosition of the Front Right module.
-        @return SwerveModulePosition
-        """
-        return SwerveModulePosition(self.FRVel.getDouble(0), self.degToRotation2d(self.FRRot.getDouble(0)))
-
-    def getRRPosition(self) -> SwerveModulePosition:
-        """
-        Gets the SwerveModulePosition of the Rear Right module.
-        @return SwerveModulePosition
-        """
-        return SwerveModulePosition(self.RRVel.getDouble(0), self.degToRotation2d(self.RRRot.getDouble(0)))
-
-    def getGyroYaw(self) -> float:
-        """
-        Gets the yaw of the gyro.
-        @return Yaw (degrees)
-        """
-        return self.gyroYaw.getDouble(0.00)
-
-    def getTargetValid(self) -> bool:
-        """
-        Determines if a target is valid.
-        @return tv
-        """
-        return self.targetValid.getBoolean(False)
-
-    def getDetectionTime(self) -> float:
-        """
-        Gets the time a detection was made.
-        @return detTimeSeconds 
-        """
-        return self.detectTime.getDouble(-1)
-
-    def getCamOffset(self) -> Transform3d:
-        """
-        Gets the camera's offset from the center of the robot.
-        @return cameraOffset
-        """
-        # Gets the offset
-        offset = self.camOffset.getDouble([0, 0, 0, 0, 0, 0])
-
-        # Return a Transform3d generated from the array
-        return Transform3d(
-            Translation3d(offset[0], offset[1], offset[2]),
-            Rotation3d(offset[3], offset[4], offset[5])
-        )
-
-    def degToRotation2d(self, degrees) -> Rotation2d:
-        """
-        Returns a Rotation2d created from degrees.
-        @param degrees
-        """
-        return Rotation2d(Units.degreesToRadians(degrees))
+        self.detectionTime.setDouble(timeSec)
--- main.py
+++ main.py
@@ -7,15 +7,9 @@ from wpimath.geometry import Pose2d
 # Import Classes
 from camera import USBCamera
 from apriltags import Detector
-from pose_estimation import PoseEstimator
-
-# Import Utilities
-from Utilities.Units import Units
-from Utilities.Logger import Logger
 
 # Instance creation
 detector  = Detector()
-robotPose = PoseEstimator()
 
 # Creates a VideoCapture and gets its properties
 camera        = USBCamera(0)
@@ -23,28 +17,8 @@ cap           = camera.autoResize()
 camresolution = camera.getResolution()
 ret, camMatrix, camdistortion, rvecs, tvecs = camera.calibrateCamera(cap)
 
-# Variables
-autoPath = None
-prevPath = None
-
-startPos = Pose2d(
-    Units.inchesToMeters(63.5),
-    Units.inchesToMeters(22),
-    Units.degreesToRadians(90)
-)
-
 # Main loop
 while (cap.isOpened() == True):
-    # Resets the starting position once the auto path is selected
-    if ((autoPath is not None) and (prevPath != autoPath)):
-        prevPath = autoPath
-
-        if (autoPath == ""):
-            robotPose.resetPoseTrackers(startPos)
-
-    # Update the pose trackers
-    robotPose.updatePoseTrackers()
-
     # Read the capture
     sucess, stream = cap.read()
 
@@ -53,12 +27,6 @@ while (cap.isOpened() == True):
 
     # Runs April Tag detection on the undistorted image
     results, stream = detector.detectTags(stream, camMatrix, 3)
-
-    # Determine Pose2d based on vision measurements
-    floorPose = robotPose.getVisionPose()
-
-    # Extract the pose values (meters and radians)
-    Logger.logDebug("X: {}, Y: {}, Yaw: {}".format(floorPose.x_feet, floorPose.y_feet, floorPose.rotation().degrees()))
 
     # Display the capture
     cv.imshow("Stream", stream)
